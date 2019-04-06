e8de3a612223ed01b5ffb766e53a894577d00a90
Fabio Chiarini <chiarrss@gmail.com>
2019-04-06 11:57:19 +0200

    Initial setup of classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ //Game class delaration
+ class Game {
+
+   //methods
+   //track the number of missed guesses by the player
+   missed() {
+
+
+   }
+
+   //create array of phrases
+   phrases() {
+
+
+   }
+
+   //phrase object that  currently in play
+   activePhrase () {
+
+   }
+
+
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,31 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ //Phrase class declaration
+ class Phrase {
+   constructor(phrase) {
+     this.phrase = phrase.toLowerCase();
+   }
+
+   //methods
+   //adds letter placeholders to the display when the game starts
+   addPhraseToDisplay() {
+
+
+   }
+
+   //checks to see if the letter selected by the player matches a letter in the phrase
+   checkLetter() {
+
+
+   }
+
+   //reveals the letter(s) on the board that matches the player's selection
+   showMatchedLetter() {
+
+   }
+
+
+
+ }
