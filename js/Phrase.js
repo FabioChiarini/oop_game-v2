7ed8eaf8c9eb9ac687af9f4217f478b46fb2e6ad
Fabio Chiarini <chiarrss@gmail.com>
2019-04-06 15:28:11 +0200

    Implemented addPhraseToDisplay function

--- js/Phrase.js
+++ js/Phrase.js
@@ -11,7 +11,21 @@
    //methods
    //adds letter placeholders to the display when the game starts
    addPhraseToDisplay() {
+     //select the correct div
+     let phraseContainer = $('#phrase ul')
+     //split the string into single letters/spaces
+     let letters = this.phrase.split("");
 
+     //format and append each letter to the ul
+     letters.forEach((letter) => {
+       //check if letter is a white space
+       if (letter !== ' ') {
+         phraseContainer.append('<li class="hide letter ' + letter + '">' + letter + '</li>');
+       }
+       else {
+         phraseContainer.append('<li class="space"> </li>');
+       }
+     });
    }
 
    //checks to see if the letter selected by the player matches a letter in the phrase
